55131834e29dbc08c37f2db0ccac980f2cf98235
Taufik Nurrahman <official.taufik980@gmail.com>
2022-03-03 13:46:19 +0700

    sesi 9

---
+++ manual/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h1>Hello World Belajar Menginstall ract manual</h1>;
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
